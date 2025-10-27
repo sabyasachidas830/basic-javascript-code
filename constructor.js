526ab7efa3b8f21d29cf6f7d360a8f44042a11ba
Sabyasachi Das <sabyasachidas830@gmail.com>
2025-10-27 09:31:45 +0530

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+const node1 = new Node(10);
+const node2 = new Node(20);
+const node3 = new Node(30);
+
+node1.next = node2;
+node2.next = node3;
+
+console.log(node1.data);
+console.log(node1.next.data);
